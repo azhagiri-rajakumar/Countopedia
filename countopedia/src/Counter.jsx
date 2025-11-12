8c7fb5df89796ad340917bbac358423e178c1ff9
Azhagiri R <azhagiri.r@ibridgellc.com>
2025-11-12 12:58:29 +0530

    [Azhagiri R] Finished CountOpedia

---
+++ countopedia/src/Counter.jsx
@@ -0,0 +1,100 @@
+import { useState, useEffect } from "react";
+import attackImg from "./images/attack.png";
+import defendImg from "./images/defend.png";
+
+function Counter() {
+  let [gameState, setGameState] = useState({
+    count: 0,
+    gameStatus: "",
+  });
+
+  function handleIncrement() {
+    const newCount = gameState.count + 1;
+    const status =
+      newCount >= 5 ? "You Won!!" : newCount <= -5 ? "You Lost!!" : "";
+    setGameState({ count: newCount, gameStatus: status });
+  }
+
+  function handleDecrement() {
+    const newCount = gameState.count - 1;
+    const status =
+      newCount >= 5 ? "You Won!!" : newCount <= -5 ? "You Lost!!" : "";
+    setGameState({ count: newCount, gameStatus: status });
+  }
+
+  function handleRandomPlay() {
+    const playMode = Math.round(Math.random());
+    if (playMode == 0) {
+      handleIncrement();
+    } else {
+      handleDecrement();
+    }
+  }
+
+  function handleReset() {
+    setGameState({ gameStatus: "", count: 0 });
+
+    // setGameState((prev) => ({
+    //   ...prev,
+    //   gameStatus: "",
+    // }));
+  }
+
+  function handleLog() {
+    console.log(count);
+  }
+
+  return (
+    <div className="container">
+      <div className="row text-white text-center">
+        <h1>Game Score : {gameState.count}</h1>
+        <p>You win at +5 points and lose at -5 points</p>
+        <p>Last Play:</p>
+        {gameState.gameStatus.length > 0 && (
+          <h3>Game Status: {gameState.gameStatus}</h3>
+        )}
+        <div className="col-6 col-md-3 offset-md-3">
+          <img
+            src={attackImg}
+            style={{
+              width: "100%",
+              cursor: "pointer",
+              border: "1px solid green",
+            }}
+            className="p-4 rounded"
+            onClick={handleIncrement}
+          ></img>
+        </div>
+        <div className="col-6 col-md-3 offset-md-3">
+          <img
+            src={defendImg}
+            style={{
+              width: "100%",
+              cursor: "pointer",
+              border: "1px solid red",
+            }}
+            className="p-4 rounded"
+            onClick={handleDecrement}
+          ></img>
+        </div>
+        <div className="col-12 col-md-4 offset-md-4">
+          <button
+            className="btn btn-success m-2 w-100"
+            onClick={handleRandomPlay}
+          >
+            Random Play
+          </button>
+          <br />
+          <button className="btn btn-danger m-2 w-100" onClick={handleReset}>
+            Reset
+          </button>
+          <button className="btn btn-warning m-2 w-100" onClick={handleLog}>
+            Log
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
